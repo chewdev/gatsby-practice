27c017dc17d7042837ed857c5b1dd05068dc7d44
chewdev <itsbewald@gmail.com>
2018-12-05 00:55:57 -0800

    Update with links to contact and about pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
+import Header from "../components/header";
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <Header text="Welcome!" />
+    <p>Where do you want to go?</p>
+    <Link to="/about">About Us</Link>
+    <Link to="/contact">Contact Us</Link>
+  </div>
+);
