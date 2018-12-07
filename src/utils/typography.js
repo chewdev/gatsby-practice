11190afe153e5c63b3d350f267b4c036ed06e8c0
chewdev <itsbewald@gmail.com>
2018-12-06 22:10:13 -0800

    Use kirkhamTheme and add rhythm export

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,4 +1,5 @@
 import Typography from "typography";
-import fairyGatesTheme from "typography-theme-fairy-gates";
-const typography = new Typography(fairyGatesTheme);
+import kirkhamTheme from "typography-theme-kirkham";
+const typography = new Typography(kirkhamTheme);
 export default typography;
+export const rhythm = typography.rhythm;
