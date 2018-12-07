ff38ea7652175e476ec24ad685fb4d6cd4bac3da
chewdev <itsbewald@gmail.com>
2018-12-06 22:05:55 -0800

    Update config to use transformer-remark, emotion, source-filesystem and add siteMetadata. Add onCreateNode and createPages to gatsby-node.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,17 @@
 module.exports = {
+  siteMetadata: {
+    title: "My Site Title!"
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
